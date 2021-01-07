4efa916fd15e729e18fdac18f5b1bb296d25c88c
alestor123 <alestoraldous@gmail.com>
2021-01-07 21:55:53 +0530

    added main code

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+var astro = require('./App'),
+chalk = require('chalk'),
+figlet = require('figlet');
+(async () => {
+var adata = await astro();
+figlet(adata.title, (err, data) => {
+console.log(chalk.yellow(data))
+console.log('Date : ' + chalk.green(adata.date))
+console.log('Description : ' + chalk.green(adata.explanation))
+console.log('URL : '+chalk.green(adata.url))
+})
+})();
